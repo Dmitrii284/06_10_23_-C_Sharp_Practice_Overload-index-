26a83e12a5277c04acfa5fe0693e55a7d1573aec
Roman Solovov <solovov.roma@bk.ru>
2023-10-06 21:19:52 +0500

    Добавьте файлы проекта.

---
+++ 06_10_23_ C_Sharp_Overload index/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_10_23__C_Sharp_Overload_index
+{
+   abstract internal class Storage
+    {
+       private string mediaName;
+       private string modelName;
+
+        public GetediaName() { return mediaName; }
+
+        }
+    public float mamoryCapaciti()
+    {
+
+    }
+
+    public void copying()
+    {
+
+    }
+
+
+    }
+    class Flash : Storage
+    {
+
+    }
+    class DVD : Storage
+    {
+
+    }
+    class HDD : Storage
+    {
+
+    }
+}
